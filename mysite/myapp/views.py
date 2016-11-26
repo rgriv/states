fc664111451d188f5b2a8434a0c8caa340151d5c
rgriv <rgrivera@uchicago.edu>
2016-11-25 21:07:33 -0600

    yup

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -23,21 +23,53 @@ def csv(request):
 
 from .forms import InputForm
 from .models import STATES_DICT, YEARS_DICT, DATA_DICT
-def make_plot(s1, s2, s3, d1, d2, d3):
+import pandas as pd
+import os
+import mpld3
+import matplotlib.pyplot as plt
+import numpy as np
+def make_plot(s1 = "None", s2 = "None", s3 = "None", d1 = "None", d2 = "None", d3 = "None"):
     # import pandas as pd
     # data_path = join(settings.STATIC_ROOT, 'myapp/FRED_LF.csv')
     # df = pd.read_csv(data_path)
     # df = df[df["County/City"].str.contains(county.lower(), case = False)
     # return
-    import pandas as pd
+    data_path1, data_path2, data_path3 = 0,0, 0
+    dlist = []
+    for f in os.listdir(join(settings.STATIC_ROOT, 'myapp/')):
+        if d1 in f and s1 != "None": data_path1 = f
+        if d2 in f and s2 != "None": data_path2 = f
+        if d3 in f and s3 != "None": data_path3 = f
+    print(data_path1, data_path2, data_path3)
+    if data_path1: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path1), usecols = ["Date", s1], index_col = "Date").rename(columns = {s1 : "{} in {}".format(DATA_DICT[d1], STATES_DICT[s1])}))
+    if data_path2: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path2), usecols = ["Date", s2], index_col = "Date").rename(columns = {s2 : "{} in {}".format(DATA_DICT[d2], STATES_DICT[s2])}))
+    if data_path3: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path3), usecols = ["Date", s3], index_col = "Date").rename(columns = {s3 : "{} in {}".format(DATA_DICT[d3], STATES_DICT[s3])}))
+    fig, ax = plt.subplots()
+    for d in dlist:
+        d.set_index(d.index.to_datetime().year, inplace = True)
+    if dlist != []:
+        df = pd.concat(dlist, axis = 1, join = "inner")
+        print(df)
+        ax.plot(df)
+        ymin, ymax = ax.get_ylim()
+        ax.set_ylim(ymin, ymax*1.2)
+        ax.legend(df.columns.values, loc=1)
+    #print(df.head())
 
-    data_path = join(settings.STATIC_ROOT, 'myapp/FRED_LF.csv')
+    #
+            #ax.plot(x = d.index.to_datetime().year, y = d[d.columns[0]])
+            #color, size = np.random.random((2, 200))
+            #fig.plot(x = [1,2,3,4], y = [1,2,3,4])
+            #ax.lines(xdata = [1,2,3,4], ydata = [1,2,3,4])
+            #ax.grid(color='lightgray', alpha=0.7)
+    return mpld3.fig_to_html(fig)
 
-    df = pd.read_csv(data_path)
-    table = df.to_html(float_format = "%.3f", classes = "table table-striped", index_names = False)
-    table = table.replace('border="1"','border="0"')
-    table = table.replace('style="text-align: right;"', "") # control this in css, not pandas.
-    return table
+    # print(s1,s2,s3,d1,d2,d3)
+    # df = pd.read_csv(data_path)
+    # table = df.to_html(float_format = "%.3f", classes = "table table-striped", index_names = False)
+    # table = table.replace('border="1"','border="0"')
+    # table = table.replace('style="text-align: right;"', "") # control this in css, not pandas.
+    #return "<p>howdy</p>"
 
 def form(request):
     # year = request.GET.get('year', '2016')
@@ -69,7 +101,7 @@ def form(request):
             #   'data1' : DATA_DICT[data1],
             #   'data2' :3DATA_DICT[data2],
             #   'data3' : DATA_DICT[data3]
-            'plot_source' : make_plot(STATES_DICT[state1], STATES_DICT[state2], STATES_DICT[state3], DATA_DICT[data1], DATA_DICT[data2], DATA_DICT[data3])
+            'plot_source' : make_plot(state1, state2, state3, data1, data2, data3)
               #'year' : YEARS_DICT[year]
               }
 
