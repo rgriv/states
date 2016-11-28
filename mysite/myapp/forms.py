c0abb0818419956622448c291aeb8068dfa5a89f
rgriv <rgrivera@uchicago.edu>
2016-11-28 11:40:41 -0600

    only a few more...

--- mysite/myapp/forms.py
+++ mysite/myapp/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-from .models import Input, STATES, YEARS, DATA
+from .models import Input, STATES, YEARS, DATA, DISPLAY_TYPES
 
 class InputForm(forms.ModelForm):
 
     attrs = {'class ' : 'form−control ',
              'onchange ' : 'this.form.submit() '}
-
+    display_type = forms.ChoiceField(choices = DISPLAY_TYPES, required = True,
+                                    widget = forms.Select(attrs = attrs))
+    start_year = forms.ChoiceField(choices = YEARS, required = True,
+                                widget = forms.Select(attrs = attrs))
+    end_year = forms.ChoiceField(choices = YEARS, required = True,
+                                widget = forms.Select(attrs = attrs))
     state1 = forms.ChoiceField(choices=STATES, required=True,
                               widget=forms.Select(attrs = attrs))
     data1 = forms.ChoiceField(choices=DATA, required=True,
@@ -17,6 +22,34 @@ class InputForm(forms.ModelForm):
     state3 = forms.ChoiceField(choices=STATES, required=True,
                               widget=forms.Select(attrs = attrs))
     data3 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state4 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data4 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state5 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data5 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state6 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data6 = forms.ChoiceField(choices=DATA, required=True,
+                                widget=forms.Select(attrs = attrs))
+    state7 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data7 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state8 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data8 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state9 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data9 = forms.ChoiceField(choices=DATA, required=True,
+                              widget=forms.Select(attrs = attrs))
+    state10 = forms.ChoiceField(choices=STATES, required=True,
+                              widget=forms.Select(attrs = attrs))
+    data10 = forms.ChoiceField(choices=DATA, required=True,
                               widget=forms.Select(attrs = attrs))
     #year = forms.ChoiceField(choices=YEARS, required=True,
     #                          widget=forms.Select(attrs = attrs))
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -6,94 +6,98 @@ from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def csv(request):
-
-   import pandas as pd
-
-   data_path = join(settings.STATIC_ROOT, 'myapp/FRED_LF.csv')
-
-   df = pd.read_csv(data_path)
-   table = df.to_html(float_format = "%.3f", classes = "table table-striped", index_names = False)
-   table = table.replace('border="1"','border="0"')
-   table = table.replace('style="text-align: right;"', "") # control this in css, not pandas.
-
-   return HttpResponse(table)
+    return HttpResponse("Hello, world. You're at the State Data Viewing index.")
 
 from .forms import InputForm
-from .models import STATES_DICT, YEARS_DICT, DATA_DICT
+from .models import STATES_DICT, YEARS_DICT, DATA_DICT, ID_DICT, DT_DICT
 import pandas as pd
 import os
 import mpld3
 import matplotlib.pyplot as plt
 import numpy as np
-def make_plot(s1 = "None", s2 = "None", s3 = "None", d1 = "None", d2 = "None", d3 = "None"):
-    # import pandas as pd
-    # data_path = join(settings.STATIC_ROOT, 'myapp/FRED_LF.csv')
-    # df = pd.read_csv(data_path)
-    # df = df[df["County/City"].str.contains(county.lower(), case = False)
-    # return
-    data_path1, data_path2, data_path3 = 0,0, 0
+def make_plot(SD_list, start_year, end_year, display_type, intersect_data):
     dlist = []
-    for f in os.listdir(join(settings.STATIC_ROOT, 'myapp/')):
-        if d1 in f and s1 != "None": data_path1 = f
-        if d2 in f and s2 != "None": data_path2 = f
-        if d3 in f and s3 != "None": data_path3 = f
-    print(data_path1, data_path2, data_path3)
-    if data_path1: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path1), usecols = ["Date", s1], index_col = "Date").rename(columns = {s1 : "{} in {}".format(DATA_DICT[d1], STATES_DICT[s1])}))
-    if data_path2: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path2), usecols = ["Date", s2], index_col = "Date").rename(columns = {s2 : "{} in {}".format(DATA_DICT[d2], STATES_DICT[s2])}))
-    if data_path3: dlist.append(pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', data_path3), usecols = ["Date", s3], index_col = "Date").rename(columns = {s3 : "{} in {}".format(DATA_DICT[d3], STATES_DICT[s3])}))
-    fig, ax = plt.subplots()
-    for d in dlist:
-        d.set_index(d.index.to_datetime().year, inplace = True)
+    for state, data in SD_list:
+        if state not in ('', 'None') and data not in ('', 'None'):
+            dlist.append((pd.read_csv(join(settings.STATIC_ROOT, 'myapp/', "{}.csv".format(data)), usecols = ["Date", state], index_col = "Date")
+                        .rename(columns = {state : "{} in {}".format(DATA_DICT[data], STATES_DICT[state])})))
+    indexes = range(int(start_year), int(end_year) + 1)
+    for d in dlist: d = d.ix[indexes]
     if dlist != []:
-        df = pd.concat(dlist, axis = 1, join = "inner")
-        print(df)
+        df = pd.concat(dlist,
+                        axis = 1,
+                        join = "inner" if intersect_data else "outer")
+    else: return "<p> Nothing To See Here </p>"
+    if display_type == "plot":
+        fig, ax = plt.subplots()
         ax.plot(df)
         ymin, ymax = ax.get_ylim()
-        ax.set_ylim(ymin, ymax*1.2)
-        ax.legend(df.columns.values, loc=1)
-    #print(df.head())
-
-    #
-            #ax.plot(x = d.index.to_datetime().year, y = d[d.columns[0]])
-            #color, size = np.random.random((2, 200))
-            #fig.plot(x = [1,2,3,4], y = [1,2,3,4])
-            #ax.lines(xdata = [1,2,3,4], ydata = [1,2,3,4])
-            #ax.grid(color='lightgray', alpha=0.7)
-    return mpld3.fig_to_html(fig)
-
-    # print(s1,s2,s3,d1,d2,d3)
-    # df = pd.read_csv(data_path)
-    # table = df.to_html(float_format = "%.3f", classes = "table table-striped", index_names = False)
-    # table = table.replace('border="1"','border="0"')
-    # table = table.replace('style="text-align: right;"', "") # control this in css, not pandas.
-    #return "<p>howdy</p>"
+        ax.set_ylim(ymin, ymax* (1 + len(df.columns)/10))
+        ax.legend(df.columns.values, loc=9)
+        ax.set_xlabel('Year')
+        return mpld3.fig_to_html(fig)
+    elif display_type == "corrplot":
+        fig, ax = plt.subplots()
+        cax = ax.matshow(df.corr())
+        fig.colorbar(cax)
+        print(df.columns.values)
+        ax.set_xticklabels(df.columns.values)
+        ax.set_yticklabels(df.columns.values)
+        ax.axis('image')
+        return mpld3.fig_to_html(fig)
+    elif display_type in ("table", "corrmatrix"):
+        if display_type == "corrmatrix": df = df.corr()
+        table = df.to_html(float_format = "%.3f", classes = "table table-striped", index_names = False)
+        table = table.replace('border="1"','border="0"')
+        table = table.replace('style="text-align: right;"', "")
+        return table
 
 def form(request):
-    #start_year = request.Get.get('start_year', 'None')
-    #end_year = request.Get.get('end_year', 'None')
     state1 = request.GET.get('state1', 'None')
     data1 = request.GET.get('data1', 'None')
     state2 = request.GET.get('state2', 'None')
     data2 = request.GET.get('data2', 'None')
     state3 = request.GET.get('state3', 'None')
     data3 = request.GET.get('data3', 'None')
+    state4 = request.GET.get('state4', 'None')
+    data4 = request.GET.get('data4', 'None')
+    state5 = request.GET.get('state5', 'None')
+    data5 = request.GET.get('data5', 'None')
+    state6 = request.GET.get('state6', 'None')
+    data6 = request.GET.get('data6', 'None')
+    state7 = request.GET.get('state7', 'None')
+    data7 = request.GET.get('data7', 'None')
+    state8 = request.GET.get('state8', 'None')
+    data8 = request.GET.get('data8', 'None')
+    state9 = request.GET.get('state9', 'None')
+    data9 = request.GET.get('data9', 'None')
+    state10 = request.GET.get('state10', 'None')
+    data10 = request.GET.get('data10', 'None')
+
+    start_year = request.GET.get('start_year', '2000')
+    end_year = request.GET.get('end_year', '2016')
+
+    display_type = request.GET.get('display_type', 'plot')
+
+    intersect_data = request.GET.get('intersect_data', bool('False'))
 
     params = {'form_action' : reverse_lazy('myapp:form'),
               'form_method' : 'get',
               'form' : InputForm({'state1' : state1, 'data1' : data1,
                                     'state2' : state2, 'data2' : data2,
-                                    'state3' : state3, 'data3' : data3}),
-            #   'state1' : STATES_DICT[state1],
-            #   'state2' : STATES_DICT[state2],
-            #   'state3' : STATES_DICT[state3],
-            #   'data1' : DATA_DICT[data1],
-            #   'data2' :3DATA_DICT[data2],
-            #   'data3' : DATA_DICT[data3]
-            'plot_source' : make_plot(state1, state2, state3, data1, data2, data3)
-              #'year' : YEARS_DICT[year]
+                                    'state3' : state3, 'data3' : data3,
+                                    'state4' : state4, 'data4' : data4,
+                                    'state5' : state5, 'data5' : data5,
+                                    'state6' : state6, 'data6' : data6,
+                                    'state7' : state7, 'data7' : data7,
+                                    'state8' : state8, 'data8' : data8,
+                                    'state9' : state9, 'data9' : data9,
+                                    'state10' : state10, 'data10' : data10,
+                                    'start_year' : start_year, 'end_year' : end_year,
+                                    'display_type' : display_type, 'intersect_data' : intersect_data}),
+            'plot_source' : make_plot( ( (state1, data1), (state2, data2), (state3, data3), (state4, data4),(state5, data5), (state6, data6),(state7, data7), (state8, data8),(state9, data9), (state10, data10) ),
+                                        start_year, end_year,
+                                        display_type, intersect_data)
               }
 
     return render(request, 'form.html', params)
